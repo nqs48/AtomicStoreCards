206b52674bf0e99cc6ad29f460a53901e54c591a
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-01 21:43:13 -0500

    Adding firebase to project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'atomic-game-6419c',
+    appId: '1:715912356968:web:26c5960b2465d38ab587dd',
+    storageBucket: 'atomic-game-6419c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAzi7Nl22fTBaCTo_3TnGW2inhtg7KLMZM',
+    authDomain: 'atomic-game-6419c.firebaseapp.com',
+    messagingSenderId: '715912356968',
+  },
   production: false,
 };
 
