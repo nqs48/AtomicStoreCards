2e9ba32b3c37b6e23ba63d00278fe4ea0e5652dd
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-06 22:20:57 -0500

    Updating auth module with firebase auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { AuthComponent } from './auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
 
 
 @NgModule({
-  declarations: [AuthComponent],
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-  ]
+    provideAuth(() => getAuth()),
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
