5c358181e533322eee650d37b97dda3d8ebf8941
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-06 22:18:46 -0500

    Updating component and molule app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,6 @@ import { ApiService } from './services/api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'atomic-game';
-  charactersList: any;
-
-  constructor(public $api : ApiService) {
-  }
-
-  ngOnInit() {
-
-    this.$api.getApi().subscribe((data: any) => {
-      this.charactersList=data.results;
-      console.log(this.charactersList);
-    }
-    );
-  }
-
-
+export class AppComponent{
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { ApiService } from './services/api.service';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,11 +29,15 @@ import { HttpClientModule } from '@angular/common/http';
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideDatabase(() => getDatabase()),
+    provideMessaging(() => getMessaging()),
+    provideFunctions(() => getFunctions()),
   ],
-  providers: [
-    ApiService
-  ],
-  bootstrap: [AppComponent]
+  providers: [ApiService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
