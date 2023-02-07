35fcd3dd4c7ce4b288c30dc884f13bf6832c2a76
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-06 23:25:12 -0500

    Adding profile-image component to atom module <dashboard>

---
+++ src/app/dashboard/components/atoms/profile-image/profile-image.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-image',
+  templateUrl: './profile-image.component.html',
+  styleUrls: ['./profile-image.component.scss'],
+})
+export class ProfileImageComponent {
+  @Input() urlImage: string | null | undefined;
+}
