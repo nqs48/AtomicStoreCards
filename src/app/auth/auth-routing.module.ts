dece35f8cd4c3c3f1e7531ffbc07f08a4d758574
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-06 22:21:23 -0500

    Setting auth routing module

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/login',
+      },
+      {
+        path: 'login',
+        pathMatch: 'full',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        pathMatch: 'full',
+        component: RegisterComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
