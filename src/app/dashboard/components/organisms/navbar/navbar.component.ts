2161fd2087d820f53f0db179722829189e9020a3
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-14 13:07:56 -0500

    Deleting button logout in navbar

--- src/app/dashboard/components/organisms/navbar/navbar.component.ts
+++ src/app/dashboard/components/organisms/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { SwalService } from 'src/app/services/notifications/swal.service';
+import { ImagesService } from '../../../../services/notifications/images.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +12,25 @@ import { Component, Input } from '@angular/core';
 export class NavbarComponent {
   @Input() src_img: string | null | undefined;
   @Input() textContentText: string | null | undefined;
+  @Input() textContentTextBalance: string | null | undefined;
   ngClassButton = 'btn btn-danger';
   ngClassText = 'text-white fs-3';
   textContentButton = 'Logout';
+  imageLogo= 'https://www.pngplay.com/wp-content/uploads/14/Rick-And-Morty-Logo-PNG-HD-Quality.png'
+
+  constructor(public $authService: AuthService,
+    public $router: Router,
+    public $swal: SwalService,
+    public $imgService: ImagesService) {}
+
+
+
+  goToUser(){
+    this.$swal.confirmationAnimated("Lets go to buy ",this.$imgService.imageToUser).then
+    (() => {
+          this.$router.navigate(['/dashboard/user']);
+    }
+    )
+  }
+
 }
