accf70af1b61d896e893d89494455c777c6d7352
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-14 13:08:59 -0500

    Updating home pages

--- src/app/dashboard/components/pages/home/home.component.ts
+++ src/app/dashboard/components/pages/home/home.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/services/auth.service';
 import { ApiService } from 'src/app/services/api.service';
 import { Router } from '@angular/router';
+import { UserModel } from 'src/app/auth/interface/user.model';
+import { CardModel } from 'src/app/auth/interface/card.model';
+import { SwalService } from '../../../../services/notifications/swal.service';
+import { ImagesService } from 'src/app/services/notifications/images.service';
 
 @Component({
   selector: 'app-home',
@@ -12,38 +16,108 @@ export class HomeComponent implements OnInit {
   ngClassButton = 'btn btn-danger';
   textContentButton = 'Logout';
 
-  title = 'atomic-game';
-  charactersList: any;
-  currentUser = this.$authService.currentUserValue();
+  charactersList!: CardModel[];
+  currentUser!: UserModel;
 
   constructor(
     public $api: ApiService,
     public $authService: AuthService,
-    public $router: Router
-  ) {}
+    public $router: Router,
+    public $swal: SwalService,
+    public $imgService: ImagesService
+  ) {
 
-  ngOnInit() {
-    this.$api.getApi().subscribe((data: any) => {
-      this.charactersList = data.results;
-      console.log(this.charactersList);
-    });
 
-    console.log(
-      'Este es el usuario actual: ',
-      this.$authService.currentUserValue()
-    );
   }
+  ngOnInit(): void {
+    this.$api.getCardsFirebase().subscribe
+   ((data: CardModel[]) => {
+     this.charactersList = data;
+    })
+
+
+    this.$authService.getUserFirestore().subscribe
+    ((data) => {
+      if(data.length == 0 ){
+        const user= this.$authService.currentUserValue();
+        this.$authService.addNewUserToFirestore(user)
+      }else{
+        this.currentUser= data[0];
+      }
+    }
+    )
+  }
+
 
   logout() {
     this.$authService
       .logout()
       .then(() => {
-        console.log('Saliendo...');
-        alert('Saliendo...');
-        this.$router.navigate(['/login']);
+        this.$swal.confirmationAnimated('The session was successfully closed',this.$imgService.imageHappy).then
+        ((result) => {
+          if (result.isConfirmed) {
+              this.$router.navigate(['/login']);
+          }
+        })
       })
       .catch((error) => {
-        console.log(error);
+        this.$swal.errorMessage("Something went wrong!!",this.$imgService.imageError);
       });
+  }
+
+  goToUser(){
+    this.$swal.confirmationAnimated('You are already in the user section',this.$imgService.imageToUser).then
+    ((result) => {
+      if (result.isConfirmed) {
+      this.$router.navigate(['/dashboard/user']);
+      }
+    }).catch
+    ((error) => {
+      this.$swal.errorMessage("Something went wrong!!",this.$imgService.imageError);
+    }
+    );
+  }
+
+  //Buy card
+  buyCard(id:any){
+      const cardBuyed: CardModel= this.charactersList.filter((card: CardModel) => card.uid === id)[0];
+
+
+      if(this.currentUser.cash < cardBuyed.price){
+        this.$swal.confirmationAnimated("You have no money \n ...top up now !", this.$imgService.imageCrying).then
+        ((result) => {
+          if (result.isConfirmed) {
+              this.$router.navigate(['/dashboard/home']);
+          }
+        }
+        )
+
+      }else if(cardBuyed.cantity < 1 ){
+        this.$swal.confirmationAnimated("This Card is not avalaible",this.$imgService.imageNotAvailable).then
+      }else{
+        this.currentUser.cash = this.currentUser.cash - cardBuyed.price;
+        this.currentUser.cards.push(cardBuyed);
+        this.$authService.updateUserFirestore(this.currentUser).then
+        ((res) => {
+          this.$swal.confirmationAnimated(`Successfully purchased card now \n you have ${this.currentUser.cash} USD`, this.$imgService.imageHappy).then
+          ((result) => {
+            if (result.isConfirmed) {
+              cardBuyed.cantity= cardBuyed.cantity - 1;
+              this.$api.updateCardFirestore(cardBuyed);
+              this.$router.navigate(['/dashboard/home']);
+
+            }
+          }
+          )
+        },
+        (err) => {
+
+          this.$swal.errorMessage("Something went wrong!!", this.$imgService.imageError)
+        }
+        );
+
+      }
+
+
   }
 }
--- src/app/dashboard/components/pages/pages.module.ts
+++ src/app/dashboard/components/pages/pages.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 import { OrganismsModule } from '../organisms/organisms.module';
 import { MoleculesModule } from '../molecules/molecules.module';
+import { AtomsModule } from '../atoms/atoms.module';
 
 
 
 @NgModule({
   declarations: [HomeComponent, UserComponent],
   imports: [
+    AtomsModule,
     CommonModule,
     MoleculesModule,
     OrganismsModule  ],
