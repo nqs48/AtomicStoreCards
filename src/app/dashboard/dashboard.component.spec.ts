32371a5562af8728dc274b6f9c3f4b3e31d232b7
Nestea Turner <nestor.quiroga@sofka.com.co>
2023-02-19 15:35:56 -0500

    Adding practice unit testing angular

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -20,4 +20,35 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
+  //Making unit test Sum
+  it('shoud sum to numbers', () => {
+    let num1= 3;
+    let num2= 4;
+    let result: number = component.sum(num1, num2);
+    expect(result).toBe(7);
+  })
+
+
+  //Sum two number with greater than 10
+  it('shoud validate is greater than 10', () => {
+      let num1= 3;
+      let num2= 4;
+      let result: boolean = component.validateSum(num1, num2);
+      expect(result).toBeFalse();
+    })
+
+
+  fit('shoul validate if return string', ()=> {
+    let num1= 9;
+    let num2= 4;
+    let result: string[] = component.addItem(num1, num2);
+    expect(result).toContain('sofka');
+  })
+
+
+
+
+
 });
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+
+  sum(num1: number, num2: number ): number{
+    return num1 + num2;
+  }
+
+  validateSum(num1:number, num2: number){
+    let sum=this.sum(num1, num2)
+    return sum > 10;
+  }
+
+
+  addItem(num1:number, num2: number): string[] {
+    let sum = this.validateSum(num1, num2);
+    return sum? ['sofka']: [''];
+   }
+
 }
